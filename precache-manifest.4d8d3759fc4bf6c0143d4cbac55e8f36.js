de11f8b7967d12ea0b0f19ca60afd23b7608aa16
Kenny <bergquist.kenneth@gmail.com>
2020-03-29 16:07:59 -0400

    Updates

---
+++ precache-manifest.4d8d3759fc4bf6c0143d4cbac55e8f36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "425c38fc198969238979fc8ba7f86e4a",
+    "url": "/covid-viz/index.html"
+  },
+  {
+    "revision": "28059422f4e873e4e8b6",
+    "url": "/covid-viz/static/css/main.c500fa28.chunk.css"
+  },
+  {
+    "revision": "dc9cc1ac44a7f6d0f290",
+    "url": "/covid-viz/static/js/2.e67e56f4.chunk.js"
+  },
+  {
+    "revision": "f32f3f2739f945f6e8e72f406ee3a70f",
+    "url": "/covid-viz/static/js/2.e67e56f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28059422f4e873e4e8b6",
+    "url": "/covid-viz/static/js/main.740cc2bc.chunk.js"
+  },
+  {
+    "revision": "d9b66737c4ca96b52300",
+    "url": "/covid-viz/static/js/runtime-main.49596344.js"
+  }
+]);
